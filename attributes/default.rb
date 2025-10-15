7c407f8323cf062123a7aba0d8c190d49d470610
bassslap <bphillip@progress.com>
2025-10-15 11:00:22 -0400

    1.0.37

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,6 +2,11 @@
 default['audit']['compliance_phase'] = true
 default['audit']['fetcher'] = 'chef-automate'
 default['audit']['reporter'] = 'chef-server-automate', 'cli'
+default['audit']['profiles'] = {
+  'elite_golf_compliance' => {
+    'compliance' => 'admin/elite_golf_compliance'
+  }
+}
 # Default attributes for elite-golf-cookbook
 
 # Common settings
