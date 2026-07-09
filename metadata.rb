c14716de903e7bbc01ce0644257cbe73561bf20a
Bryan Phillips <62438110+bassslap@users.noreply.github.com>
2026-07-09 15:05:43 -0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Elite Golf Team'
 maintainer_email 'admin@elitegolf.com'
 license 'Apache-2.0'
 description 'Deploys a simple golf-themed web application'
-version '1.0.53'
+version '1.0.54'
 chef_version '>= 16.0'
 
 supports 'windows'
